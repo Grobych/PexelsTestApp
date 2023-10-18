84b7d8f5cfd3eea1ac3e90767b7130b6ef516ed1
Alexander Globa <grobych@gmail.com>
2023-10-18 13:01:36 +0300

    update app theme

--- ui/src/main/java/com/globa/pexelstestapp/ui/theme/Color.kt
+++ ui/src/main/java/com/globa/pexelstestapp/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.globa.pexelstestapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Red = Color(0xFFBB1020)
+val White = Color(0xFFFFFFFF)
+val Gray = Color(0xFFF3F5F9)
+val DarkGray = Color(0xFF868686)
+val Black = Color(0xFF1E1E1E)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Dark_Red = Color(0xFFBB1020)
+val Dark_White = Color(0xFFFFFFFF)
+val Dark_Gray = Color(0xFF393939)
+val Dark_DarkGray = Color(0xFFB5B5B5)
+val Dark_Black = Color(0xFF1E1E1E)
